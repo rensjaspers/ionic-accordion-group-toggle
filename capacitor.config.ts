ac58b82707e1b9837f65f2d1f8cec18696acc8f1
Rens Jaspers <rens.jaspers@gmail.com>
2024-06-19 14:47:53 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-accordion-group-toggle',
+  webDir: 'www'
+};
+
+export default config;
